5882e41ff9b064f5168b95b96c6269bef1f36dc5
Tim Zaman <timbobel@gmail.com>
2020-01-05 18:47:34 -0800

    Add threads, socket and semaphores in a working system.

--- pt/main.py
+++ pt/main.py
@@ -6,48 +6,81 @@ import torch.utils.cpp_extension
 
 print()
 
-op = torch.utils.cpp_extension.load(name="op_cpp", sources=["op.cpp"])
+
+class TVLHandle:
+    def __init__(self, tvl):
+        print('TVLHandle')
+        self._tvl = tvl
+        self.handle = None
+
+    def __enter__(self):
+        print('__enter__')
+        self.handle = self._tvl.backend.get_handle()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        print('__exit__')
+        self._tvl.backend.release_handle(self.handle)
+        self.handle = None
+
+    @property
+    def input_buffer(self):
+        return self._tvl.input_buffers[self.handle]
+
+    @property
+    def output_buffer(self):
+        return self._tvl.output_buffers[self.handle]
+
+    def remote_decode(self, data):
+        print(f'remote_decode with self.handle={self.handle}')
+        return self._tvl.backend.remote_decode(self.handle, data)
 
 
-# frames = op.decode(b'blabla', [1])
-# print(f'frames={frames}')
+class TVL:
+    op = torch.utils.cpp_extension.load(name="op_cpp", sources=["op.cpp"])
+
+    def __init__(self, handles):
+        # To avoid complexity, we use torch's to manage our (shared) memory. For transparency,
+        # this is defined here in python so we can easily debug things. We pass it to the C++
+        # operation later so that it can access its contents.
+        self.input_buffers = []
+        self.output_buffers = []
+        for i in range(handles):
+            # Create one input and output shared memory buffer per handle
+            self.input_buffers.append(torch.zeros([1000000], dtype=torch.uint8).share_memory_())
+            self.output_buffers.append(torch.zeros([4, 2], dtype=torch.uint8).share_memory_())
+
+        # Share the shared memory buffers with the C++ op so that it can access it.
+        self.backend = self.op.TVL(handles, self.input_buffers, self.output_buffers)
+
+    def get_handle(self):
+        return TVLHandle(tvl=self)
 
 
-# This should instantiate the TVL, which starts C++ worker threads.
-a = op.MyClass(1)
+tvl = TVL(handles=2)
 
-# time.sleep(5)
 
-x = a.debug()
-# print(f'x={x}')
-# exit()
 
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self):
         super(MyDataset).__init__()
 
     def __len__(self):
-        return 10
+        return 32
 
     def __getitem__(self, i):
+        with tvl.get_handle() as h:
+            encoded_data = bytes(f'__getitem__({i})'.encode('utf8'))
+            frames = h.remote_decode(encoded_data)
+            # print(f'frames={frames}')
+        
         # time.sleep(1)
-        a.remote_decode(b'1337', [i])
-
-        # Create an empty buffer to put the files into (preferably pinned and shared)
-        # buffer = torch.tensor([32]).share_memory_()
-
-        a.remote_decode(buffer=buffer)
 
         return i
 
 
-def worker_init_fn(i):
-    print(f'worker_init_fn({i}) pid={os.getpid()}')
-    # worker_info = torch.utils.data.get_worker_info()
-    # print(f'a={a} a.debug()={a.debug()}')
-
 dataset = MyDataset()
-loader = torch.utils.data.DataLoader(dataset, num_workers=1, batch_size=2, worker_init_fn=worker_init_fn)
+loader = torch.utils.data.DataLoader(dataset, num_workers=2, batch_size=2)
 
 
 for i in loader:
--- pt/op.cpp
+++ pt/op.cpp
@@ -7,8 +7,6 @@
 #include <unistd.h>  // usleep
 #include <sys/socket.h>
 
-const char* SEM_NAME = "my_semaphore";
-
 
 torch::Tensor decode(py::bytes data, std::vector<int> frame_indices) {
     // This is the actual stand-along(blocking) decode functionality,
@@ -25,97 +23,159 @@ torch::Tensor decode(py::bytes data, std::vector<int> frame_indices) {
 }
 
 
+static const int parentsocket = 0;
+static const int childsocket = 1;
+
+struct packet {
+    int n_input_bytes;  // Amount of bytes in the encoded packet
+};
 
 
 
-
-
-class MyClass {
+class TVL {
   public:
 
-    void worker_thread() {
-        std::cout << "worker_thread())" <<  std::endl;
+    void worker_thread(int handle) {
+        // There are as many worker threads as there are handles.
+        std::cout << "worker_thread(handle=" << handle << ")" <<  std::endl;
+        const int socket = this->socketpair_fds[handle][parentsocket];
 
-        printf("calling sem_wait\n");
-        if(sem_wait(this->sem_) < 0) {
-            printf("sem_wait error\n");
+        while (1) {
+
+            packet p;
+            int n = read(socket, &p, sizeof(packet));
+            std::cout << ::getpid() << " Received packet n_input_bytes=" << p.n_input_bytes << std::endl;
+
+            auto input_buffer = this->input_buffers_[handle];
+            // Obtain the pointer to the shared memory input buffer.
+            uint8_t * ptr = input_buffer.data_ptr<uint8_t>();
+
+            // Print out the contents of the shared memory input buffer.
+            std::cout << "Received contents: ";
+            for (int i=0; i<p.n_input_bytes; i++) {
+                std::cout << ptr[i];
+            }
+            std::cout << std::endl;
+
+            // HEAVY CUDA PROCESSING HAPPENS HERE
+            usleep(2000000); // Fake processing
+            auto output_buffer = this->output_buffers_[handle];
+            output_buffer[0] = 13;
+            output_buffer[1] = 37;
+
+            // Send back to the child that the processing has completed.
+
+            std::cout << ::getpid() << " Sending packet:" << p.n_input_bytes << std::endl;
+
+            int status = 0;
+            write(socket, &status, sizeof(int));
         }
-        printf("sem was set!!!\n");
-        // Perform processing here?
-        usleep(3000000);
-        sem_post(this->sem_);
+
     }
 
-    MyClass(int number) {
-        std::cout << "MyClass::MyClass(" << number << ")" << std::endl;
+    std::vector<std::array<int, 2>> socketpair_fds;
+    std::vector<sem_t *> semaphores_;
 
-        /* initialize semaphores for shared processes */
-        this->ctor_pid_ = ::getpid();
-        std::cout << "ctor pid=" << ::getpid() <<std::endl;
-        unsigned int sem_value = 1; 
+    int handles_;
+    std::vector<torch::Tensor> input_buffers_;
+    std::vector<torch::Tensor> output_buffers_;
+    
+    
+    TVL(const int handles, const  std::vector<torch::Tensor> &input_buffers, const std::vector<torch::Tensor> &output_buffers) {
+        std::cout << "ctor pid=" << ::getpid()<< " TVL::TVL(handles=" << handles << ")" << std::endl;
 
-        // Create a named semaphore.
-        sem_unlink(SEM_NAME);  // Total hack.
-        this->sem_ = sem_open(SEM_NAME, O_CREAT | O_EXCL, 0644, sem_value);
-        if (this->sem_ == SEM_FAILED) {
-            std::cout << "Error creating semaphore=" << this->sem_ << std::endl;
+        handles_ = handles;
+        input_buffers_ = input_buffers;
+        output_buffers_ = output_buffers;
+
+        this->socketpair_fds.resize(handles);
+        this->semaphores_.resize(handles);
+
+        for (int i=0; i<handles; i++){
+            // Create the socket pairs.
+            socketpair(PF_LOCAL, SOCK_STREAM, 0, this->socketpair_fds[i].data());
+
+            // Create the worker threads.
+            std::thread t(&TVL::worker_thread, this, i);
+            t.detach();  // Who cares about cleanup.
+
+            // Create the named semaphores. These indicate if users have a handle on the system.
+            // TODO(tzaman): just use unnamed semaphores when on UNIX.
+            std::string sem_name = "tvl_sem_" + std::to_string(i);
+            sem_unlink(sem_name.c_str());  // Total hack.
+            unsigned int sem_value = 1;
+            sem_t * sem = sem_open(sem_name.c_str(), O_CREAT | O_EXCL, 0644, sem_value);
+            if (sem == SEM_FAILED) {
+                std::cout << "Error creating semaphore=" << sem << std::endl;
+            }
+            this->semaphores_[i] = sem;
+        }
+    }
+
+    ~TVL() {
+        std::cout << "TVL::~TVL()" << std::endl;
+    }
+
+    int get_handle() {
+        std::cout << "TVL::get_handle()" << std::endl;
+
+        while (1) {
+            // Try all semaphores until we find a free one
+            for (int i=0; i < this->handles_; i++){
+                sem_t * sem = this->semaphores_[i];
+                if(sem_trywait(sem) < 0) {  // Improve this terrible spinlock
+                    // perror("sem_wait");
+                    // usleep(10000);
+                } else {
+                    std::cout << "Got handle on" << i << std::endl;
+                    return i;
+                }
+            }
+
         }
 
-        // Create an unnamed semaphore (not supported on MacOS!)
-        // int ret = sem_init(this->sem_, 0, sem_value);
-        // if (ret) {
-        //     std::cout << "sem_init error ret=" << ret << std::endl;
-        // }
-
-
-
-        std::thread t(&MyClass::worker_thread, this);
-        t.detach();  // I don't give a shit about cleanup.
-        
-
-
-
-    }
-    ~MyClass() {
-        // TODO(tzaman): cleaning up could be tricky, as we likely lost track of who's using
-        // the semaphore XD.
-        std::cout << "MyClass::~MyClass()" << std::endl;
-        // if (::getpid() == this->ctor_pid_){
-        //     // The creator process should clean up any semaphores.
-        //     sem_unlink(SEM_NAME);   
-        //     sem_close(this->sem_);  
-        // }
-    }
-    int ctor_pid_;
-    sem_t* sem_ = nullptr;
-    std::string debug(){
-        std::cout << "MyClass::debug()"  <<std::endl;
-        std::cout << "@debug pid=" << ::getpid() <<std::endl;
-        int val = -1;
-        // int ret = sem_getvalue(this->sem_, &val);
-        // if (ret) {
-        //     std::cout << "@debug ret=" << ret << std::endl;
-        // }
-        std::cout << "@debug sem_=" << this->sem_ << " val=" << val << std::endl;
-
-        // if (sem_post(this->sem_) == -1) {
-        //     printf("sem_post() failed\n");
-        // }
-        
-
-        return "Foobar";
+        return 0;
     }
 
-    // torch::Tensor remote_decode(py::bytes data, std::vector<int> frame_indices) {
-    torch::Tensor remote_decode(py::bytes data) {
-        // This should be used if the caller is not the main process and hence has no CUDA
-        // context. It will communicate with the main process (with the CUDA context) through ipc.
-        std::cout << "remote_decode()" << std::endl;
-        worker_thread();
-        auto frames = torch::zeros({0});
-        return {
-            frames
-        };
+    void release_handle(const int handle) {
+        std::cout << "TVL::release_handle(" << handle << ")" << std::endl;
+        sem_t * sem = this->semaphores_[handle];
+        if (sem_post(sem) == -1) {
+            perror("sem_post");
+        }
+    }
+
+    torch::Tensor remote_decode(const int handle, const py::bytes &data) {
+
+        std::cout << "remote_decode(handle=" << handle << ")" << std::endl;
+
+        torch::Tensor input_buffer = this->input_buffers_[handle];
+
+        // Convert the bytes data
+        std::string s = data;
+        int n_input_bytes = s.size();
+        for (int i=0; i < n_input_bytes; i++){
+            input_buffer[i] = s[i];
+        }
+
+        // int64_t * ptr = x.data_ptr<int64_t>();
+
+        const int socket = this->socketpair_fds[handle][childsocket];
+
+        packet p;
+        p.n_input_bytes = n_input_bytes;
+
+        std::cout << ::getpid() << " Sending packet:" << p.n_input_bytes << std::endl;
+        write(socket, &p, sizeof(packet));
+
+        // Read the status back
+        int status;
+        int n = read(socket, &status, sizeof(int));
+
+        std::cout << "status=" << status << std::endl;
+
+        torch::Tensor output_buffer = this->output_buffers_[handle];
+        return output_buffer;
     }
 
 };
@@ -123,8 +183,9 @@ class MyClass {
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("decode", &decode, "decode");
-    py::class_<MyClass>(m, "MyClass")
-        .def(py::init<int>())
-        .def("debug", &MyClass::debug)
-        .def("remote_decode", &MyClass::remote_decode, "decode");
+    py::class_<TVL>(m, "TVL")
+        .def(py::init<int, std::vector<torch::Tensor>, std::vector<torch::Tensor>>())
+        .def("get_handle", &TVL::get_handle)
+        .def("release_handle", &TVL::release_handle)
+        .def("remote_decode", &TVL::remote_decode, "decode");
 }
